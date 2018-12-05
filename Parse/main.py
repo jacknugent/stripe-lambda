dd6e661cdebed678a8ab238acd7db9939bbe1419
Jack Nugent <jacknugent@Jacks-MacBook-Pro.local>
2018-12-05 12:59:46 -0500

    passing through metadata also

--- Parse/main.py
+++ Parse/main.py
@@ -31,15 +31,11 @@ def handler(event, context):
     for record in event['Records']:
         bucket = record['s3']['bucket']['name'] #stripes-in-film
         key = record['s3']['object']['key'] #test-image.jpg
-        
         response = s3.head_object(Bucket=bucket, Key=key)
-
         logger.info('Response: {}'.format(response))
-
-        print(response['Metadata']) #contains all the metadata
-        
+        # print(response['Metadata']) #contains all the metadata {'approved': '0', 'description': 'asfasfasf', 'title': 'A Star Is Born (2018)'}
         download_path = '/tmp/{}{}'.format(uuid.uuid4(), key) #/tmp/f62fc7ef-10e0-4a49-81e6-6827e663e62dtest_image.jpg
         upload_path = '/tmp/resized-{}'.format(key) #/tmp/resized-test_image.jpg
         s3.download_file(bucket, key, download_path)
         resize_image(download_path, upload_path)
-        s3.upload_file(upload_path, bucket.replace('submitted', 'clean'), key)
+        s3.upload_file(upload_path, bucket.replace('submitted', 'clean'), key, ExtraArgs={'Metadata': response['Metadata']})
