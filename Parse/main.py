d139ffe877d770786e7362773d60a237f87f7e09
Jack Nugent <jacknugent@Jacks-MacBook-Pro.local>
2018-12-06 14:04:54 -0500

    ransomware hack

--- Parse/main.py
+++ Parse/main.py
@@ -2,37 +2,55 @@ from __future__ import print_function
 import boto3
 import uuid
 from PIL import Image
-import logging
+from PIL import ImageChops
 
 s3 = boto3.client('s3')
-logger = logging.getLogger()
-logger.setLevel(logging.INFO)
-# def resize_image(image_path, resized_path):
-#     with Image.open(image_path) as image:
-#         if image.format == "png":
-#                 print("png found")
-#                 convert = Image.new("RGB", image.size, (255,255,255))
-#                 convert.paste(image, (0,0), image)
-#                 convert.save(resized_path.replace("png", "jpg"), quality=95)
-#         else:
-#                 image.save(resized_path)
-#conversion currently does not work, need to test locally
 
-     
-def resize_image(image_path, resized_path):
+def resize_image(image_path, resized_path, response):
+    """crop/compress image and add it's metadata to dict
+
+    :param image_path: path of the original file
+    :param resized_path: path for the cropped/compressed file
+    :param response: metadata
+    :return: none
+    """
+
     with Image.open(image_path) as image:
-        image.thumbnail(tuple(x / 2 for x in image.size))
-        image.save(resized_path)
-                     
+        # if a png, convert to a jpg
+        if image.format == "PNG":
+            image = image.convert('RGB')
+
+        #get black borders and remove them from the image
+        bg = Image.new(image.mode, image.size, color=0)
+        diff = ImageChops.difference(image, bg)
+        bbox = diff.getbbox()
+        cropped = image.crop(bbox)
+
+        #save new image size, save image
+        response['width'] = str(cropped.size[0])
+        response['height'] = str(cropped.size[1])
+        cropped.save(resized_path, quality=97)
+
 def handler(event, context):
+    """
+    move image from stripes-submitted and move to stripes-clean
+
+    :param event: file upload event
+    :param context:
+    :return: nothing
+    """
     for record in event['Records']:
-        bucket = record['s3']['bucket']['name'] #stripes-in-film
-        key = record['s3']['object']['key'] #test-image.jp
-        response = s3.head_object(Bucket=bucket, Key=key)
-        logger.info('Response: {}'.format(response))
-        # print(response['Metadata']) #contains all the metadata {'approved': '0', 'description': 'asfasfasf', 'title': 'A Star Is Born (2018)'}
-        download_path = '/tmp/{}{}'.format(uuid.uuid4(), key) #/tmp/f62fc7ef-10e0-4a49-81e6-6827e663e62dtest_image.jpg
-        upload_path = '/tmp/resized-{}'.format(key) #/tmp/resized-test_image.jpg
+        bucket = record['s3']['bucket']['name']
+        key = record['s3']['object']['key']
+
+        #contains all the metadata
+        response = s3.head_object(Bucket=bucket, Key=key)['Metadata']
+
+        download_path = '/tmp/{}{}'.format(uuid.uuid4(), key)
+        upload_path = '/tmp/resized-{}'.format(key).replace("png", "jpg")
+
         s3.download_file(bucket, key, download_path)
-        resize_image(download_path, upload_path)
-        s3.upload_file(upload_path, bucket.replace('submitted', 'clean'), key, ExtraArgs={'Metadata': response['Metadata']})
+        resize_image(download_path, upload_path, response)
+        s3.upload_file(upload_path, bucket.replace('submitted', 'clean'), key, ExtraArgs={'Metadata': response})
+
+# resize_image('test.png', 'result.jpg', {})
